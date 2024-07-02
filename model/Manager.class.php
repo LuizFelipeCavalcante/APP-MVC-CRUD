e26eee66634b984ba4d9eda8fb10855fd2fb8f1e
Arthur Freitas <12202444@aluno.cotemig.com.br>
2024-07-02 10:43:46 -0300

    100%_updated

--- model/Manager.class.php
+++ model/Manager.class.php
@@ -4,8 +4,8 @@
         public function insert_client($data){
             $pdo = parent::get_instance();
 
-            $sql = "INSERT INTO usuario VALUES (NULL, nome, email, cpf, data_nascimento, telefone, endereco"
-            $statement $pdo->prepare($sql);
+            $sql = "INSERT INTO usuario VALUES (NULL, nome, email, cpf, data_nascimento, telefone, endereco)";
+            $statement = $pdo->prepare($sql);
             foreach ($data as $key => $value) {
             $statement->bindValue(":$key", $value);
 }
